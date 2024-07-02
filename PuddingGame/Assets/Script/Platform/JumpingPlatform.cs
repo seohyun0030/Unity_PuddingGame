66c8ec594615cddd0bbc5cf5a9d85be13e1e3cdd
pm2979 <pm29798512@gmail.com>
2024-07-03 04:26:26 +0900

    pm

---
+++ PuddingGame/Assets/Script/Platform/JumpingPlatform.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingPlatform : MonoBehaviour //튀어오르는 발판
+{
+    public float bounce = 5f; // 튀어오르는 힘
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ContactPoint2D contactPoint = collision.contacts[0];
+        Vector2 pos = contactPoint.point; // 충돌 지점
+        Vector2 normal = contactPoint.normal; // 법선 벡터
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(-normal.normalized * bounce, ForceMode2D.Impulse);
+        }
+    }
+
+}
