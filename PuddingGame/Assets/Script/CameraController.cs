424616c66e85b79e6c8fe8386e9598d04c050fd2
seohyun0030 <163545492+seohyun0030@users.noreply.github.com>
2024-07-23 20:34:10 +0900

    카메라

--- PuddingGame/Assets/Script/CameraController.cs
+++ PuddingGame/Assets/Script/CameraController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float cameraSpeed = 5.0f;
+    public float cameraSpeed = 3.0f;
 
     public GameObject player;
 
@@ -13,10 +13,11 @@ public class CameraController : MonoBehaviour
         Vector3 dir = player.transform.position - this.transform.position;
 
         //점프할 때도 카메라 따라감
-        //Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
+        Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
+        this.transform.Translate(moveVector);
 
         //점프할 때 카메라 안따라감
-        Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, 0.0f, 0.0f);
-        this.transform.Translate(moveVector);
+        /*Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, 0.0f, 0.0f);
+        this.transform.Translate(moveVector);*/
     }
 }
