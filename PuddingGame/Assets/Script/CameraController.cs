be9c26171e895db7e37d805a1b22789b846e9b69
seohyun0030 <163545492+seohyun0030@users.noreply.github.com>
2024-08-14 16:14:08 +0900

    방향키 카메라 이동 구현

--- PuddingGame/Assets/Script/CameraController.cs
+++ PuddingGame/Assets/Script/CameraController.cs
@@ -14,10 +14,21 @@ public class CameraController : MonoBehaviour
 
         //점프할 때도 카메라 따라감
         Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
-        this.transform.Translate(moveVector);
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+            this.transform.Translate(moveVector);
 
         //점프할 때 카메라 안따라감
         /*Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, 0.0f, 0.0f);
         this.transform.Translate(moveVector);*/
+
+        CameraMove();
+    }
+    void CameraMove()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveHorizontal * cameraSpeed * Time.deltaTime, moveVertical * cameraSpeed * Time.deltaTime, 0.0f);
+        this.transform.Translate(move);
     }
 }
