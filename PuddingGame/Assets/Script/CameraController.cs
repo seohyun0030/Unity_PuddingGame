8351180fc92710f8003d1176f5fd2ad3a4a659dc
seohyun0030 <163545492+seohyun0030@users.noreply.github.com>
2024-09-30 21:31:25 +0900

    카메라 이동 연출

--- PuddingGame/Assets/Script/CameraController.cs
+++ PuddingGame/Assets/Script/CameraController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    Camera camera;
     public float cameraSpeed;
     private PlayerMoveControl playerMoveControl;
     public GameObject player;
@@ -13,32 +16,44 @@ public class CameraController : MonoBehaviour
 
     public float playerXPos;
     public float playerYPos;
+
+    public float zoomOut;
+    public float zoomSpeed;
+    public float animMoveSpeed;
+    public Vector3 dest_1;
+    public Vector3 dest_2;
+    public Vector3 dest_3;
+    public bool isAnimation;
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+        StartCoroutine(Stage1Animation());
+    }
     private void FixedUpdate()
     {
-        playerMoveControl = player.GetComponent<PlayerMoveControl>();
-        Vector3 dir = player.transform.position - this.transform.position;
-
-
-        if (!playerMoveControl.matcha)
+        if(!isAnimation)
         {
-            //점프할 때도 카메라 따라감
-            Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
+            playerMoveControl = player.GetComponent<PlayerMoveControl>();
+            Vector3 dir = player.transform.position - this.transform.position;
 
-            if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
-                this.transform.Translate(moveVector);
-        }
-        else
-        {
-            Vector3 matchaFollowVector = new Vector3(dir.x, dir.y, 0.0f);
-            this.transform.Translate(matchaFollowVector * cameraSpeed * Time.deltaTime);
-        }
-        //점프할 때 카메라 안따라감
-        /*Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, 0.0f, 0.0f);
-        this.transform.Translate(moveVector);*/
 
-        CameraMove();
-        //LimitPosition();
-        LimitPositionPlayer();
+            if (!playerMoveControl.matcha)
+            {
+                //점프할 때도 카메라 따라감
+                Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
+
+                if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+                    this.transform.Translate(moveVector);
+            }
+            else
+            {
+                Vector3 matchaFollowVector = new Vector3(dir.x, dir.y, 0.0f);
+                this.transform.Translate(matchaFollowVector * cameraSpeed * Time.deltaTime);
+            }
+
+            CameraMove();
+            LimitPositionPlayer();
+        }
     }
     void CameraMove()
     {
@@ -50,17 +65,6 @@ public class CameraController : MonoBehaviour
 
         this.transform.Translate(move);
         
-    }
-    void LimitPosition()    //카메라 이동 제한
-    {
-        Vector3 position = transform.position;
-
-        // 영역을 벗어나지 않도록 제한
-        position.x = Mathf.Clamp(position.x, minPos.x, maxPos.x);
-        position.y = Mathf.Clamp(position.y, minPos.y, maxPos.y);
-
-        // 제한된 위치로 업데이트
-        transform.position = position;
     }
     void LimitPositionPlayer()  //플레이어의 위치에 따라 카메라 이동 제한
     {
@@ -82,5 +86,87 @@ public class CameraController : MonoBehaviour
 
         // 제한된 위치로 업데이트
         transform.position = position;
+    }
+    public IEnumerator Stage1Animation()
+    {
+        //연출이 실행중을 나타냄
+        isAnimation = true;
+        float elapsedTime = 0f;
+        float currentSize = camera.orthographicSize;
+        float zoomOutTime = Mathf.Abs(zoomOut - currentSize) / zoomSpeed;
+        float distance;
+        //줌 아웃이 다 될 때까지 줌 아웃을 한다
+        while (elapsedTime < zoomOutTime)
+        {
+            camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, zoomOut, Time.deltaTime * zoomSpeed);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        MoveAnimation(dest_1);
+        /*//이동하는 시간동안 dest_1까지 이동을 한다
+        float moveTime = Vector3.Distance(transform.position, dest_1) / animMoveSpeed;
+        elapsedTime = 0f;
+        while (elapsedTime < moveTime)
+        {
+            distance = Vector3.Distance(transform.position, dest_1);  //거리 구하기
+            if (distance < 0.2f)        //현재 위치와 타겟 위치의 거리가 0.2보다 작으면 이동 멈추기
+            {
+                break;
+            }
+            transform.position = Vector3.Lerp(transform.position, dest_1, elapsedTime / moveTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = dest_1;
+
+        //이동하는 시간동안 dest_2까지 이동을 한다
+        moveTime = Vector3.Distance(transform.position, dest_2) / animMoveSpeed;
+        elapsedTime = 0f;
+        while (elapsedTime < moveTime)
+        {
+            distance = Vector3.Distance(transform.position, dest_2);  //거리 구하기
+            if (distance < 0.2f)        //현재 위치와 타겟 위치의 거리가 0.2보다 작으면 이동 멈추기
+            {
+                break;
+            }
+            transform.position = Vector3.Lerp(transform.position, dest_2, elapsedTime / moveTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = dest_2;
+
+        //이동하는 시간동안 dest_3까지 이동을 한다
+        moveTime = Vector3.Distance(transform.position, dest_3) / animMoveSpeed;
+        elapsedTime = 0f;
+        while (elapsedTime < moveTime)
+        {
+            distance = Vector3.Distance(transform.position, dest_3);  //거리 구하기
+            if (distance < 0.2f)        //현재 위치와 타겟 위치의 거리가 0.2보다 작으면 이동 멈추기
+            {
+                break;
+            }
+            transform.position = Vector3.Lerp(transform.position, dest_3, elapsedTime / moveTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = dest_3;*/
+
+        //연출이 끝남을 알림
+        isAnimation = false;
+    }
+    void MoveAnimation(Vector3 dest)
+    {
+        float moveTime = Vector3.Distance(transform.position, dest) / animMoveSpeed;
+        float elapsedTime = 0f;
+        while (elapsedTime < moveTime)
+        {
+            float distance = Vector3.Distance(transform.position, dest);  //거리 구하기
+            if (distance < 0.2f)        //현재 위치와 타겟 위치의 거리가 0.2보다 작으면 이동 멈추기
+            {
+                break;
+            }
+            transform.position = Vector3.Lerp(transform.position, dest, elapsedTime / moveTime);
+            elapsedTime += Time.deltaTime;
+        }
     }
 }
