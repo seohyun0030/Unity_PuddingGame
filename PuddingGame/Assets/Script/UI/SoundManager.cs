1945c504fb564180450312cbe32324c3945d6513
pm2979 <pm29798512@gmail.com>
2024-12-25 14:19:59 +0900

    UI

--- PuddingGame/Assets/Script/UI/SoundManager.cs
+++ PuddingGame/Assets/Script/UI/SoundManager.cs
@@ -2,32 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
-public enum EAudioMixerType { Master, BGM, SFX }
+
 public class SoundManager : MonoBehaviour
 {
-    public static SoundManager Instance;
+    public Slider BGMSlider;
+    public Slider SFXSlider;
     [SerializeField] private AudioMixer audioMixer;
 
     private float[] audioVolumes = new float[3];
     private void Awake()
-    {
-        Instance = this;
+    {   
+      BGMSlider.onValueChanged.AddListener(ChangeBGM);
     }
 
-    public void SetAudioVolume(EAudioMixerType audioMixerType, float volume)
+    void Start()
     {
-        audioMixer.SetFloat(audioMixerType.ToString(), Mathf.Log10(volume) * 20);
+        if (PlayerPrefs.HasKey("Volume"))
+        {
+            BGMSlider.value = PlayerPrefs.GetFloat("Volume");
+        }
+        else
+            BGMSlider.value = 0.5f;
+
+        if (PlayerPrefs.HasKey("Volume_2"))
+        {
+            SFXSlider.value = PlayerPrefs.GetFloat("Volume_2");
+        }
+        else
+            SFXSlider.value = 0.5f;
+
+        audioMixer.SetFloat("BGM", Mathf.Log10(BGMSlider.value) * 20);
+        audioMixer.SetFloat("BGM", Mathf.Log10(SFXSlider.value) * 20);
     }
+
 
     public void ChangeBGM(float volume)
     {
-        SoundManager.Instance.SetAudioVolume(EAudioMixerType.BGM, volume);
+        audioMixer.SetFloat("BGM" , Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("Volume", BGMSlider.value);
     }
 
     public void ChangeSFX(float volume)
     {
-        SoundManager.Instance.SetAudioVolume(EAudioMixerType.SFX, volume);
+        audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("Volume_2", SFXSlider.value);
     }
 }
 
